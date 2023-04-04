ce719c52bc01ee2da2baf565f29c49094857c971
mine <junghee0204@naver.com>
2023-04-04 23:58:55 +0900

    feat: 채팅방 기능 구현 (CRUD) #mine

---
+++ TogeDutch/src/main/java/com/proj/togedutch/controller/ChatRoomController.java
@@ -0,0 +1,46 @@
+package com.proj.togedutch.controller;
+
+import com.proj.togedutch.config.BaseException;
+import com.proj.togedutch.config.BaseResponse;
+import com.proj.togedutch.domain.ChatRoom;
+import com.proj.togedutch.dto.ChatRoomDto;
+import com.proj.togedutch.service.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chatRoom")
+public class ChatRoomController {
+    private final ChatRoomService chatRoomService;
+
+    // 채팅방 생성
+    @PostMapping("")
+    public BaseResponse<ChatRoomDto> createChatRoom(){
+        ChatRoomDto chatRoomDto = chatRoomService.createChatRoom();
+        return new BaseResponse<>(chatRoomDto);
+    }
+
+    // 모든 채팅방 리스트 조회
+    @GetMapping("")
+    public BaseResponse<List<ChatRoomDto>> getAllChatRooms(){
+        List<ChatRoomDto> getChatRoomsRes = chatRoomService.getAllChatRooms();
+        return new BaseResponse<>(getChatRoomsRes);
+    }
+
+    //채팅방 하나 조회
+    @GetMapping("/{chatRoom_id}")
+    public BaseResponse<ChatRoomDto> getChatRoomById(@PathVariable("chatRoom_id") int chatRoomIdx){
+        ChatRoomDto chatRoomDto = chatRoomService.getChatRoomById(chatRoomIdx);
+        return new BaseResponse<>(chatRoomDto);
+    }
+
+    // 채팅방 삭제
+    @DeleteMapping("/{chatRoom_id}")
+    public BaseResponse<Integer> deleteChatRoom (@PathVariable("chatRoom_id") int chatRoomIdx) {
+        chatRoomService.deleteChatRoom(chatRoomIdx);
+        return new BaseResponse<>(1);
+    }
+}
---
+++ TogeDutch/src/main/java/com/proj/togedutch/domain/ChatRoom.java
@@ -0,0 +1,30 @@
+package com.proj.togedutch.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@NoArgsConstructor
+@DynamicInsert
+@Entity
+@Table(name="ChatRoom")
+public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="chatRoom_id")
+    private int chatRoomIdx;
+    @Column(name="created_at")
+    private Timestamp createdAt;
+
+    @Builder
+    public ChatRoom(int chatRoomIdx, Timestamp createdAt) {
+        this.chatRoomIdx = chatRoomIdx;
+        this.createdAt = createdAt;
+    }
+}
